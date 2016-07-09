42ac9fa71b96d7463d51d3fe9368a89e6f97623c
mlding <Mengli.Ding@wnco.com>
2016-07-09 14:02:26 +0800

    add lichenqiang directory

---
+++ lichenqiang/b.java
@@ -0,0 +1,8 @@
+/**
+ * Created by mlding on 7/9/16.
+ */
+public class b {
+    public static void main(String[] args) {
+        System.out.println('b');
+    }
+}
---
+++ yuxuan/test/aTest.java
@@ -0,0 +1,20 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by mlding on 7/9/16.
+ */
+public class aTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testMain() throws Exception {
+
+    }
+}
